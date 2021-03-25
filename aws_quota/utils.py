d1456071d67d5b5be55caca982ca4b8216ee8055
Max Brenner <xamrennerb@gmail.com>
2021-03-25 22:53:14 +0100

    make Python 3.7 compatible

--- aws_quota/utils.py
+++ aws_quota/utils.py
@@ -2,6 +2,6 @@ import functools
 import boto3
 
 
-@functools.lru_cache
+@functools.lru_cache()
 def get_account_id(session: boto3.Session) -> str:
     return session.client('sts').get_caller_identity()['Account']
